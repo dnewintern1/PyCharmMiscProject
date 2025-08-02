857dcba33170c608334e3e86e28de45eb1e9d3a4
radhe <radhe3127@gmail.com>
2025-08-02 21:57:03 +0530

    getting started

---
+++ script.py
@@ -0,0 +1,51 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#day = input("what day it is? ")
+#print(day)
+#print(type(day))
+#print(f"Hello, nice to meet you? the day ")
+
+#Type casting++++++++++++++++++++=
+
+#input score
+#score = input("what was your score? ")
+##type(score)
+#print(score * 2)
+#score = "100"
+#print(type(score))
+
+#score= int(100)
+#print(type(score))
+
+#days_per_week = 7
+#type(str(days_per_week))
+#sentence = "the number of days in a week " + str(days_per_week)
+#print(sentence)
+
+####################################
+#type casting
+#number_of_moons=str(79) # first solution using type casting
+number_of_moons=(79)
+#type(str(number_of_moons))
+sentence= "the total no. of mmoons on the planet is " + str(number_of_moons) ## second solution using type casting
+print(sentence)
+
+#ex 2
+age_in_years = int(input("what is your age? "))
+age_in_Months = age_in_years * 12
+print("your age in months is ", age_in_Months)
+
