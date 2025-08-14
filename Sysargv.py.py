2b27db648ec36f88acf484a47b9ce11393b8665c
radhe <radhe3127@gmail.com>
2025-08-14 21:25:05 +0530

    Introduction to argparse Running a python script on terminal by calling the py script from terminal
    helloworld.py -- displaying content from shell by deciding the no. of times a program needs to be called
    
    making scripts and running them from shell
    
    sys.argv[1] -- similar to input('somekkey') -- taking input from the user but that in shell

---
+++ Sysargv.py.py
@@ -0,0 +1,6 @@
+#%%
+import sys
+argument_list = sys.argv
+print("script has the name ", '"',(sys.argv[0]))
+print("Number of argument is ", '"',(len(sys.argv)))
+print("The argument ", '"',argument_list)
---
+++ addition.py
@@ -0,0 +1,4 @@
+import sys
+arg1 = int(sys.argv[1])
+arg2 = int(sys.argv[2])
+print('Addition of the two number is', arg1 + arg2)
